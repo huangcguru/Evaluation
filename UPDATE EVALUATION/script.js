03fe8b740b14c2a6edc6c51736dd515baad5bad1
Chloe <huangcguru@gmail..com>
2022-06-02 09:58:28 -0600

    fix the error

--- UPDATE EVALUATION/script.js
+++ UPDATE EVALUATION/script.js
@@ -1,70 +1,104 @@
-const contentContainer = document.querySelector('.content-container')
-const submitBtn = document.querySelector('.search-form__button')
-const inputBox = document.querySelector('.search-form__text')
+const contentContainer = document.querySelector(".content-container");
+const submitBtn = document.querySelector(".search-form__button");
+const inputBox = document.querySelector(".search-form__text");
 
-let album = []
-// let loading = false;
+let album = [];
+
+var loader = document.getElementById("loader");
+
+function displayLoading() {
+  loader.style.visibility = "visible";
+}
+
+function hideLoading() {
+  loader.style.visibility = "hidden";
+}
 
 const fetchData = (artist) => {
-    fetchJsonp(`https://itunes.apple.com/search?term=${artist}&media=music&entity=album&attribute=artistTerm&limit=200`)
-    .then(res=>res.json())
-    .then(res=>{
-        album = res.results
-        renderDisplay()
+  displayLoading();
+  fetchJsonp(
+    `https://itunes.apple.com/search?term=${artist}&media=music&entity=album&attribute=artistTerm&limit=200`
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      hideLoading();
+      album = res.results;
+      renderDisplay();
     })
-    .catch(()=>{
-        alert('Oops.. artisit does not exsit ')
-    })
-}
+    .catch((error) => {
+      console.log(error, "error");
+      alert("Oops.. artisit does not exsit");
+    });
+};
 
-const submit = () =>{
-    submitBtn.addEventListener('click',()=>{
-        loadingDisplay()
-        if(inputBox.value === "" || inputBox.value === null){
-            alert("Input area can't be empty")
-            return
-        }
-        let artists = inputBox.value
-        fetchData(artists)
-    })
-}
-submit()
+submitBtn.addEventListener("click", function (e) {
+  let artisitName = inputBox.value;
 
-const renderDisplay = () =>{
-    let render = album.map(arr=>{
-        return (`<div class="content-container"> 
+  if (artisitName === "") {
+    alert("Please type a name");
+    return;
+  } else {
+    fetchData(artisitName);
+  }
+});
+
+const renderDisplay = () => {
+  let render = album
+    .map((arr) => {
+      return `<div class="content-container">
                     <ul>
-                        <li><img src=${arr.artworkUrl60} alt="album-picture" class="album-img"/>
-                        <li>Artist: <b style="color:green">${arr.artistName}</b></li>
+                        <li><img src=${
+                          arr.artworkUrl60
+                        } alt="album-picture" class="album-img"/>
+                        <li>Artist: <b style="color:green">${
+                          arr.artistName
+                        }</b></li>
                         <li>Album Name: <b style="color:red">${arr.collectionName.toUpperCase()}</b></li>
-                        <li>Album Price: <b style="color:blue">$${arr.collectionPrice}</b></li>
-                        <li><button class="delete-btn" id="${arr.collectionId}">Close</button></li>
+                        <li>Album Price: <b style="color:blue">$${
+                          arr.collectionPrice
+                        }</b></li>
+                        <li><button class="delete-btn" id="${
+                          arr.collectionId
+                        }">Close</button></li>
                     </ul>
                 </div>
-        `)
-    }).join('')
-    setTimeout(()=>{
-        deleteAlbumBtn()
-    },1000)
-    contentContainer.innerHTML = render
-}
+        `;
+    })
+    .join("");
+  setTimeout(() => {
+    deleteAlbumBtn();
+  }, 1000);
+  contentContainer.innerHTML = render;
+};
 
-const loadingDisplay = () =>{
-    var loading = document.getElementById ( "loader" ) ;
-    loading.style.visibility = "visible" 
-    
-    // still ned to figure out to do it right...
-    setTimeout(()=>{
-        loading.style.visibility = "hidden" 
-    },500)
-//     if ( page_request.readyState == 1 )
-//       loading.style.visibility = "visible" ;
+// var loader = document.getElementById("loader");
 
-//   // when loaded successfully
-//   if (page_request.readyState == 4 && (page_request.status==200 || window.location.href.indexOf("http")==-1))
-//   {
-//       document.getElementById(containerid).innerHTML=page_request.responseText ;
-//       loading.style.visibility = "hidden" ;
-//   }
-}
+// function displayLoading() {
+//   loading.style.visibility = "visible";
+//   setTimeout(() => {
+//     loading.style.visibility = "hidden";
+//   }, 5000);
+// }
 
+// function hideLoading() {
+//   loading.style.visibility = "hidden";
+// }
+
+// const loadingDisplay = () => {
+//   var loading = document.getElementById("loader");
+//   loading.style.visibility = "visible";
+
+//   // still ned to figure out to do it right...
+//   setTimeout(() => {
+//     loading.style.visibility = "hidden";
+//   }, 500);
+//   //     if ( page_request.readyState == 1 )
+//   //       loading.style.visibility = "visible" ;
+
+//   //   // when loaded successfully
+//   //   if (page_request.readyState == 4 && (page_request.status==200 || window.location.href.indexOf("http")==-1))
+//   //   {
+//   //       document.getElementById(containerid).innerHTML=page_request.responseText ;
+//   //       loading.style.visibility = "hidden" ;
+//   //   }
+// };
